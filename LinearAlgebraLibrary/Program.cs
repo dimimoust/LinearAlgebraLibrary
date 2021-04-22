5a8b89ad5fbe158881468abcbd9589ce8ba144f9
Dimitra Moustogianni <dim.moust8@gmail.com>
2021-04-22 15:33:18 +0300

    Check SSOR method (doesnot converge)

--- LinearAlgebraLibrary/Program.cs
+++ LinearAlgebraLibrary/Program.cs
@@ -1,20 +1,27 @@
 ﻿using System;
+using System.Diagnostics;
 using LinearAlgebra.Solvers;
 using LinearAlgebraLibrary.Matrices;
+using LinearAlgebraLibrary.Solvers;
 using LinearAlgebraLibrary.Utilities;
 
 namespace LinearAlgebraLibrary
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            
-            double[,] matrix3 =  { { 10, 1, 1 },
-                                 { 1, 10, 1 },
-                                 { 1, 1, 10 } };
-            var matrix1 = new DenseMatrix(matrix3);
-            var isDominant=matrix1.IsDiagonallyDominant(0);
+
+            double[] actualSolution1 = { -2.875, 0.1875, 1.375 };
+            double[] b = { 3, 4, 2 };
+            double[,] matrix3 = { { 1, 2, 4 }, { 2, 8, 6 }, { 4, 6, 9 } };
+            SSOR ssor = new SSOR();
+            var (solution, _, _) = ssor.SSORMethod(matrix3, b, 1000, 10e-3, 1.3);
+
+            //for (int i = 0; i < b.Length; i++)
+            //{
+            //    Debug.Assert(Math.Abs(solution[i] - actualSolution1[i]) < 10e-4);
+            //}
         }
     }
 }
---
+++ LinearAlgebraLibrary/Solvers/SSOR.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinearAlgebraLibrary.Solvers
+{
+    public class SSOR
+    {
+        public (double[] x, double residual, int iterations) SSORMethod(double[,] A, double[] b, int maxIterations,
+            double accuracy, double w)
+        {
+
+            int rows = A.GetLength(0);
+            int columns = A.GetLength(1);
+            double residual = 0.0;
+
+            double[] initialGuess = new double[b.Length];
+            double[] x = initialGuess;
+            double[] x1_2 = initialGuess;
+            double[] y = new double[b.Length];
+            int iterations = 0;
+
+            while (residual <= accuracy)
+                //for (int t = 1; t < maxIterations; t++)
+                //{
+                for (int i = 0; i < rows; i++)
+                {
+                    y[i] = 0;
+                    for (int j = 0; j < i; j++)
+                    {
+                        y[i] = y[i] + A[i, j] * x1_2[j];
+                    }
+
+                    for (int j = i + 1; j < columns; j++)
+                    {
+                        y[i] = y[i] + A[i, j] * x[j];
+                    }
+
+                    y[i] = (b[i] - y[i]) / A[i, i];
+
+                    x1_2[i] = x[i] + w * (y[i] - x[i]);
+                }
+
+            for (int i = rows - 1; i >= 0; i--)
+            {
+                y[i] = 0;
+                for (int j = 0; j < i; j++)
+                {
+                    y[i] = y[i] + A[i, j] * x1_2[j];
+                }
+
+                for (int j = i + 1; j < columns; j++)
+                {
+                    y[i] = y[i] + A[i, j] * x[j];
+                }
+
+                y[i] = (b[i] - y[i]) / A[i, i];
+
+                x[i] = x1_2[i] + w * (y[i] - x1_2[i]);
+            }
+            // iterations = t;
+
+            double[] result = new double[b.Length];
+            for (int i = 0; i < rows; i++)
+            {
+                var sum = 0.0;
+                for (int j = 0; j < columns; j++)
+                {
+                    sum += A[i, j] * x[j];
+                    result[i] = b[i] - sum;
+                }
+            }
+
+            for (int i = 0; i < columns; i++)
+            {
+                residual += Math.Pow(result[i], 2);
+            }
+            residual = Math.Sqrt(residual);
+            //}
+
+            return (x, residual, iterations);
+        }
+    }
+}
+
